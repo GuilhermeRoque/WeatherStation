7d032bd076fd0a0ab23d629d2f6099ac1c06de17
Marcone <marcone.p.louzada@gmail.com>
2019-12-02 18:50:57 -0300

    update main
    Signed-off-by: Marcone <marcone.p.louzada@gmail.com>

--- main.cpp
+++ main.cpp
@@ -35,7 +35,7 @@ LCD display = LCD();
 
 Timer timer = Timer(1000);
 
-char info;
+char info, intervalo;
 uint16_t single;
 
 
@@ -65,31 +65,65 @@ void handle_fsm(int event){
             else if(event == Command){
                 if (info == '1'){
                     display.LCD_Clear();
-                    display.LCD_String("Valor digitado: ");
-                    display.LCD_Char(info);   
+                    display.LCD_String("Valor digitado:");
+                    display.LCD_Char(info);
                     _state = Conf;
                 }
-                if (info == '2'){
+                else if (info == '2'){
                     display.LCD_Clear();
-                    display.LCD_String("Valor digitado: ");
+                    display.LCD_String("Valor digitado:");
                     display.LCD_Char(info);   
                     _state = Get;
                 }
-                if (info == '3'){
+                else if (info == '3'){
                     display.LCD_Clear();
-                    display.LCD_String("Valor digitado: ");
+                    display.LCD_String("Valor digitado:");
                     display.LCD_Char(info);   
                     _state = Synck;
+                }
+                else{
+                    display.LCD_Clear();
+                    display.LCD_String("     Valor  ");
+                    display.LCD_Command(0xC0);		/* Go to 2nd line*/
+                    display.LCD_String("   Inválido!  ");
+                    _delay_ms(2000);  // só para teste
+                    display.LCD_Clear();
                 }
             }
             break;            
 
 
         case Conf:
-            uart.puts("Case Conf");
-            _delay_ms(2000);
-            _state = Idle;
-            break;
+            uart.puts("\nCase Conf");
+                display.LCD_Clear();
+                display.LCD_String(" Digite o valor");
+                display.LCD_Command(0xC0);		/* Go to 2nd line*/
+                display.LCD_String("  do intervalo");
+                _delay_ms(1000);
+                display.LCD_Clear();
+                display.LCD_String(" Digite o valor");
+                display.LCD_Command(0xC0);		/* Go to 2nd line*/
+                display.LCD_String("   em minutos");
+                _delay_ms(1000);
+                while(true){
+                    display.LCD_Clear();
+                    display.LCD_String("Aguardando...");
+                    _delay_ms(1000);
+                    if(!uart.has_data()){
+                        continue;
+                    }
+                    intervalo = uart.get();
+                    uart.put(intervalo);
+                    display.LCD_Clear();
+                    display.LCD_String("   Intervalo");
+                    display.LCD_Command(0xC0);		/* Go to 2nd line*/
+                    display.LCD_String("  Digitado: ");
+                    display.LCD_Char(intervalo);
+                    _delay_ms(2000);
+                    break;
+                }
+                _state = Idle;
+                break;
 
 
         case Get:
