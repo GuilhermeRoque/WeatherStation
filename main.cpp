9f1caf834e2c5ed22abcfc6dfe99c2cd9c3dcf5d
GuilhermeRoque <roqueee007@gmail.com>
2019-12-02 09:30:44 -0300

    FSM

--- main.cpp
+++ main.cpp
@@ -19,6 +19,11 @@ enum States {Idle,
              Synck
 };
 
+enum Event {
+    Read,
+    Command,
+};
+
 int _state;
 
 //Iniciando a serial
@@ -30,80 +35,96 @@ LCD display = LCD();
 
 Timer timer = Timer(1000);
 
+char info;
+uint16_t single;
 
-void handle_fsm(){
-}
 
-int main(int argc, char** argv) {
-    char info;
-    uint16_t single;
-    sei(); //Ativa interrupção global
-    display.LCD_Init();
-    _state = Idle;
-	timer.addTimeout(2000, &handle_fsm);
-    while(1){
-        display.LCD_Clear();
-        if(uart.has_data()){
-            info = uart.get();
-            uart.put(info);
-            if (info == '1'){
-                display.LCD_Clear();
-                display.LCD_String("Valor digitado: ");
-                display.LCD_Char(info);   
-                _state = Conf;
-            }
-            if (info == '2'){
-                display.LCD_Clear();
-                display.LCD_String("Valor digitado: ");
-                display.LCD_Char(info);   
-                _state = Get;
-            }
-            if (info == '3'){
-                display.LCD_Clear();
-                display.LCD_String("Valor digitado: ");
-                display.LCD_Char(info);   
-                _state = Synck;
+void handle_fsm(int event){
+
+    display.LCD_Clear();
+    switch(_state){
+        
+        if(event == Command){
+            if(!uart.has_data()){
+                return;
             }
             else{
-                display.LCD_String("     Valor  ");
-                display.LCD_Command(0xC0);		/* Go to 2nd line*/
-                display.LCD_String("   Inválido!  ");
-                _delay_ms(2000);  // só para teste
-                display.LCD_Clear();
+                info = uart.get();
+                uart.put(info);
             }
         }
         
-        switch(_state){
-            case Idle:
+        case Idle:
+            if(event == Read){
                 uart.puts("Case Idle");
                 single = adc.single_read(ADConverter::A0);
                 display.LCD_String("L: ");	
                 convert(single);
                 _delay_ms(1000);
-                break;            
-            
-                
-            case Conf:
-                uart.puts("Case Conf");
-                _delay_ms(2000);
-                _state = Idle;
-                break;
-            
-                
-            case Get:
-                uart.puts("Case Get");
-                _delay_ms(2000);
-                _state = Idle;
-                break;
-            
-                
-            case Synck:                
-                uart.puts("Case Synck");
-                _delay_ms(2000);
-                _state = Idle;
-                break;
-        }
+            }
+            else if(event == Command){
+                if (info == '1'){
+                    display.LCD_Clear();
+                    display.LCD_String("Valor digitado: ");
+                    display.LCD_Char(info);   
+                    _state = Conf;
+                }
+                if (info == '2'){
+                    display.LCD_Clear();
+                    display.LCD_String("Valor digitado: ");
+                    display.LCD_Char(info);   
+                    _state = Get;
+                }
+                if (info == '3'){
+                    display.LCD_Clear();
+                    display.LCD_String("Valor digitado: ");
+                    display.LCD_Char(info);   
+                    _state = Synck;
+                }
+            }
+            break;            
+
+
+        case Conf:
+            uart.puts("Case Conf");
+            _delay_ms(2000);
+            _state = Idle;
+            break;
+
+
+        case Get:
+            uart.puts("Case Get");
+            _delay_ms(2000);
+            _state = Idle;
+            break;
+
+
+        case Synck:                
+            uart.puts("Case Synck");
+            _delay_ms(2000);
+            _state = Idle;
+            break;
     }
+};
+
+void event_Read(){
+    handle_fsm(Read);
+}
+void event_Command(){
+    handle_fsm(Command);
+}
+
+int main(int argc, char** argv) {
+    sei(); //Ativa interrupção global
+    display.LCD_Init();
+    _state = Idle;
+	timer.addTimeout(60000, &event_Read);
+	timer.addTimeout(1000, &event_Command);
+
+    while (true){
+        timer.timeoutManager();
+    }
+
     return 0;
 }
 
